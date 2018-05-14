94456d8f5c3bde55e42f22348074dcad18d66e85
Mike Baglio Jr <mbag102@gmail.com>
2018-05-14 10:19:28 -0400

    Update example-run.sh

--- example-run.sh
+++ example-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull ictu/zap2docker-weekly
+docker pull mbagliojr/zap2docker
 docker run --rm -v $(pwd):/zap/wrk/:rw -t ictu/zap2docker-weekly zap-baseline-custom.py -r testreport.html -g gen.conf -d -m 5 \
 -t http://www.website.com \
 --auth_loginurl "http://www.website.com/login" \
